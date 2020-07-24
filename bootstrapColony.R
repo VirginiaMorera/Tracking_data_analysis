5e2a4251e8fe3bc499cd47574e597bd549fb2bfd
VirginiaMorera <morera.virginia@gmail.com>
2020-07-24 19:04:32 +0200

    homogenisation of function and argument names
    Changed function and argument names to conform to lowerCamelCase

--- bootstrapColony.R
+++ bootstrapColony.R
@@ -1,16 +1,16 @@
 ### bootstrap colony effect ####
 
 # Tracks: Tracking data. DF or SPDF, containing Latitude, Longitude, Species and Population
-# SimulatedDistributions: DF or SPDF containing the simulated distribution of the studied 
+# simulatedDistributions: DF or SPDF containing the simulated distribution of the studied 
 #   species, with at least the columns Longitude, Latitude, Population and Species. Can be directly the output of the simulateDistribution() function
 # tripID: unique identifier for each trip 
 # UDLev: Numeric, Utilisation Distribution Level. Level of the KDE to be selected. For example, if 50 is used, the overlap is calculated between the 
 #   smallest areas for which the probability to find the animal is equal to 0.50 (but see explanation for argument "conditional". From the adehabitatHR::kerneloverlap() function.
-# Scale is the smoothing factor to be used in the Kernel Density Estimation (in Km)
+# scale is the smoothing factor to be used in the Kernel Density Estimation (in Km)
 # Grid is a number giving the size of the grid on which the UD should be estimated. 
-# Iterations:  Numeric, number of iterations for the bootstrap
+# iterations:  Numeric, number of iterations for the bootstrap
 
-bootstrapColony <- function(Tracks, SimulatedDistributions, tripID, UDLev = 50, Scale, Grid = 500, Iterations = 50){
+bootstrapColony <- function(tracks, simulatedDistributions, tripId, udLev = 50, scale, grid = 500, iterations = 50){
   require(sp)
   require(geosphere)
   require(rgdal)
@@ -19,98 +19,98 @@ bootstrapColony <- function(Tracks, SimulatedDistributions, tripID, UDLev = 50, 
   require(doParallel)
   require(parallel)
   
-  Tracks$tripID <- Tracks[,tripID]
-  if (!"Latitude" %in% names(Tracks)) stop("Latitude field does not exist")
-  if (!"Longitude" %in% names(Tracks)) stop("Longitude field does not exist")
-  if (!"tripID" %in% names(Tracks)) stop("tripID field does not exist")
-  if (!"Population" %in% names(Tracks)) stop("Population field does not exist")
-  if (!"Species" %in% names(Tracks)) stop("Sp field does not exist")
+  tracks$tripId <- tracks[,tripId]
+  if (!"Latitude" %in% names(tracks)) stop("Latitude field does not exist")
+  if (!"Longitude" %in% names(tracks)) stop("Longitude field does not exist")
+  if (!"tripId" %in% names(tracks)) stop("tripId field does not exist")
+  if (!"Population" %in% names(tracks)) stop("Population field does not exist")
+  if (!"Species" %in% names(tracks)) stop("Sp field does not exist")
   
   # select species corresponding to the Tracking data
-  SimulatedDistributions <- SimulatedDistributions[SimulatedDistributions$Species == unique(Tracks$Species),]
+  simulatedDistributions <- simulatedDistributions[simulatedDistributions$Species == unique(tracks$Species),]
   
-  # Converts SimulatedDistributions to spatial dataframe and projects it (and if already is SPDF check projection)
-  if (class(SimulatedDistributions) != "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
+  # Converts simulatedDistributions to spatial dataframe and projects it (and if already is SPDF check projection)
+  if (class(simulatedDistributions) != "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
   {
     ## filter DF to the minimum fields that are needed
-    CleanSimulatedDistributions <- SimulatedDistributions %>%
+    CleansimulatedDistributions <- simulatedDistributions %>%
       dplyr::select(Species, Population, Longitude, Latitude)
-    mid_point <- data.frame(centroid(cbind(CleanSimulatedDistributions$Longitude, CleanSimulatedDistributions$Latitude)))
+    mid_point <- data.frame(centroid(cbind(CleansimulatedDistributions$Longitude, CleansimulatedDistributions$Latitude)))
     
     ### PREVENT PROJECTION PROBLEMS FOR DATA SPANNING DATELINE
-    if (min(CleanSimulatedDistributions$Longitude) < -170 &  max(CleanSimulatedDistributions$Longitude) > 170) {
-      longs = ifelse(CleanSimulatedDistributions$Longitude < 0,CleanSimulatedDistributions$Longitude + 360,CleanSimulatedDistributions$Longitude)
+    if (min(CleansimulatedDistributions$Longitude) < -170 &  max(CleansimulatedDistributions$Longitude) > 170) {
+      longs = ifelse(CleansimulatedDistributions$Longitude < 0,CleansimulatedDistributions$Longitude + 360,CleansimulatedDistributions$Longitude)
       mid_point$lon <- ifelse(median(longs) > 180,median(longs) - 360,median(longs))}
     
-    SimulatedDistributions.Wgs <- SpatialPoints(data.frame(CleanSimulatedDistributions$Longitude, CleanSimulatedDistributions$Latitude), proj4string = CRS("+proj=longlat + datum=wgs84"))
+    simulatedDistributions.Wgs <- SpatialPoints(data.frame(CleansimulatedDistributions$Longitude, CleansimulatedDistributions$Latitude), proj4string = CRS("+proj=longlat + datum=wgs84"))
     proj.Sim <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep = ""))
-    SimulatedDistributions.Projected <- spTransform(SimulatedDistributions.Wgs, CRS = proj.Sim)
-    SimulatedDistributionsSpatial <- SpatialPointsDataFrame(SimulatedDistributions.Projected, data = CleanSimulatedDistributions)
-    SimulatedDistributionsSpatial@data <- SimulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
-    SimulatedDistributions.Wgs <- NULL
-    SimulatedDistributions.Projected <- NULL
+    simulatedDistributions.Projected <- spTransform(simulatedDistributions.Wgs, CRS = proj.Sim)
+    simulatedDistributionsSpatial <- SpatialPointsDataFrame(simulatedDistributions.Projected, data = CleansimulatedDistributions)
+    simulatedDistributionsSpatial@data <- simulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
+    simulatedDistributions.Wgs <- NULL
+    simulatedDistributions.Projected <- NULL
     
   } else {## if data are already in a SpatialPointsDataFrame then check for projection 
-    if (is.na(SimulatedDistributions@proj4string)) stop("The proj4string slot of SimulatedDistributions can't be empty. Check and assign projection and re-run")
-    if (!is.projected(SimulatedDistributions)) { # if it's not projected (lonlat) project to laea around midpoint
-      mid_point <- data.frame(centroid(cbind(SimulatedDistributions@coords[,1], SimulatedDistributions@coords[,2])))
+    if (is.na(simulatedDistributions@proj4string)) stop("The proj4string slot of simulatedDistributions can't be empty. Check and assign projection and re-run")
+    if (!is.projected(simulatedDistributions)) { # if it's not projected (lonlat) project to laea around midpoint
+      mid_point <- data.frame(centroid(cbind(simulatedDistributions@coords[,1], simulatedDistributions@coords[,2])))
       proj.Sim <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep = ""))
       ### MB  This part prevents projection problems around the DATELINE 
-      if (min(SimulatedDistributions@coords[,1]) < -170 &  max(SimulatedDistributions@coords[1,]) > 170) {
-        longs = ifelse(SimulatedDistributions@coords[,1] < 0, SimulatedDistributions@coords[,1] + 360, SimulatedDistributions@coords[,1])
+      if (min(simulatedDistributions@coords[,1]) < -170 &  max(simulatedDistributions@coords[1,]) > 170) {
+        longs = ifelse(simulatedDistributions@coords[,1] < 0, simulatedDistributions@coords[,1] + 360, simulatedDistributions@coords[,1])
         mid_point$lon <- ifelse(median(longs) > 180, median(longs) - 360, median(longs))}
-      SimulatedDistributionsSpatial <- spTransform(SimulatedDistributions, proj.Sim)
-      SimulatedDistributionsSpatial@data <- SimulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
+      simulatedDistributionsSpatial <- spTransform(simulatedDistributions, proj.Sim)
+      simulatedDistributionsSpatial@data <- simulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
     } else {  ## if projected, "unproject" to WGS to project laea around midpoint
-      SimulatedDistributions.WGS <- spTransform(SimulatedDistributions, CRS("+proj=longlat + datum=wgs84"))
-      mid_point <- data.frame(centroid(cbind(SimulatedDistributions.WGS@coords[,1], SimulatedDistributions.WGS@coords[,2])))
+      simulatedDistributions.WGS <- spTransform(simulatedDistributions, CRS("+proj=longlat + datum=wgs84"))
+      mid_point <- data.frame(centroid(cbind(simulatedDistributions.WGS@coords[,1], simulatedDistributions.WGS@coords[,2])))
       
       ### MB  This part prevents projection problems around the DATELINE 
-      if (min(SimulatedDistributions.WGS@coords[,1]) < -170 &  max(SimulatedDistributions.WGS@coords[1,]) > 170) {
-        longs = ifelse(SimulatedDistributions@coords[,1] < 0, SimulatedDistributions@coords[,1] + 360, SimulatedDistributions@coords[,1])
+      if (min(simulatedDistributions.WGS@coords[,1]) < -170 &  max(simulatedDistributions.WGS@coords[1,]) > 170) {
+        longs = ifelse(simulatedDistributions@coords[,1] < 0, simulatedDistributions@coords[,1] + 360, simulatedDistributions@coords[,1])
         mid_point$lon <- ifelse(median(longs) > 180, median(longs) - 360, median(longs))}
       
       proj.Sim <- CRS(paste("+proj=laea +lon_0=", mid_point$lon, " +lat_0=", mid_point$lat, sep = ""))
-      SimulatedDistributionsSpatial <- spTransform(SimulatedDistributions.WGS, CRS = proj.Sim)
-      SimulatedDistributionsSpatial@data <- SimulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
+      simulatedDistributionsSpatial <- spTransform(simulatedDistributions.WGS, CRS = proj.Sim)
+      simulatedDistributionsSpatial@data <- simulatedDistributionsSpatial@data %>% dplyr::select(Species, Population)
     }
   }
   
-  # Converts Tracks to spatial dataframe and projects it (and if already is SPDF check projection)
-  if (class(Tracks) != "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
+  # Converts tracks to spatial dataframe and projects it (and if already is SPDF check projection)
+  if (class(tracks) != "SpatialPointsDataFrame")     ## convert to SpatialPointsDataFrame and project
   {
     ## filter DF to the minimum fields that are needed
-    CleanTracks <- Tracks %>%
-      dplyr::select(Species, Population, tripID, Latitude, Longitude)
+    CleanTracks <- tracks %>%
+      dplyr::select(Species, Population, tripId, Latitude, Longitude)
     
     Tracks.Wgs <- SpatialPoints(data.frame(CleanTracks$Longitude, CleanTracks$Latitude), proj4string = CRS("+proj=longlat + datum=wgs84"))
-    Tracks.Projected <- spTransform(Tracks.Wgs, CRS = SimulatedDistributionsSpatial@proj4string)
+    Tracks.Projected <- spTransform(Tracks.Wgs, CRS = simulatedDistributionsSpatial@proj4string)
     TracksSpatial <- SpatialPointsDataFrame(Tracks.Projected, data = CleanTracks)
-    TracksSpatial@data <- TracksSpatial@data %>% dplyr::select(Species, Population, tripID)
+    TracksSpatial@data <- TracksSpatial@data %>% dplyr::select(Species, Population, tripId)
     Tracks.Wgs <- NULL
     Tracks.Projected <- NULL
     
   } else {## if data are already in a SpatialPointsDataFrame then check for projection
-    if (is.na(Tracks@proj4string)) stop("The proj4string slot of Tracks can't be empty. Check and assign projection and re-run")
-    TracksSpatial <- spTransform(Tracks, CRS = SimulatedDistributionsSpatial@proj4string)
-    TracksSpatial@data <- TracksSpatial@data %>% dplyr::select(Species, Population, tripID)
+    if (is.na(tracks@proj4string)) stop("The proj4string slot of tracks can't be empty. Check and assign projection and re-run")
+    TracksSpatial <- spTransform(tracks, CRS = simulatedDistributionsSpatial@proj4string)
+    TracksSpatial@data <- TracksSpatial@data %>% dplyr::select(Species, Population, tripId)
   }
     
   
-  # remove tripID tracks with < 6 points as they can't be used to calculate kernel
-  UIDs <- names(which(table(TracksSpatial@data[, tripID]) > 6))             
-  TracksSpatial <- TracksSpatial[TracksSpatial@data[, tripID] %in% UIDs, ]
-  TracksSpatial@data[ ,tripID] <- droplevels(as.factor(TracksSpatial@data[ ,tripID]))
+  # remove tripId tracks with < 6 points as they can't be used to calculate kernel
+  UIDs <- names(which(table(TracksSpatial@data[, tripId]) > 6))             
+  TracksSpatial <- TracksSpatial[TracksSpatial@data[, tripId] %in% UIDs, ]
+  TracksSpatial@data[ ,tripId] <- droplevels(as.factor(TracksSpatial@data[ ,tripId]))
   
   TracksSpatial$X <- TracksSpatial@coords[,1]
   TracksSpatial$Y <- TracksSpatial@coords[,2]
   
-  UIDs <- as.character(unique(TracksSpatial$tripID))
+  UIDs <- as.character(unique(TracksSpatial$tripId))
   Ntrips <- length(UIDs)
   Nloop <- seq(1,(Ntrips - 1),ifelse(Ntrips > 100,10,1))
-  DoubleLoop <- data.frame(SampleSize = rep(Nloop,each = Iterations), Iteration = rep(seq(1:Iterations),length(Nloop)))
+  DoubleLoop <- data.frame(SampleSize = rep(Nloop,each = iterations), Iteration = rep(seq(1:iterations),length(Nloop)))
   LoopNr <- seq(1:dim(DoubleLoop)[1])	
-  UDLev <- UDLev
+  # udLev <- UDLev
   
   #setup parallel backend to use 4 processors
   cl <- makeCluster(detectCores(), outfile = "")
@@ -134,19 +134,19 @@ bootstrapColony <- function(Tracks, SimulatedDistributions, tripID, UDLev = 50, 
     sink("D:/selected_ids_colony_bootstrap.txt", append = TRUE)
     cat(RanNum, "\n", "\n")
     sink()
-    SelectedCoords <- TracksSpatial[TracksSpatial@data[,tripID] %in% RanNum,]
+    SelectedCoords <- TracksSpatial[TracksSpatial@data[,tripId] %in% RanNum,]
     # Ext <- (min(SelectedCoords@coords[,1]) + 3 * diff(range(SelectedCoords@coords[,1])))
-    # if(Ext < (Scale * 1000 * 2)) {
-    # BExt <- ceiling((Scale * 1000 * 3)/(diff(range(SelectedCoords@coords[,1])))) #} else {BExt <- 3}
-    KDE.Surface <- kernelUD(SelectedCoords, h = Scale*1000, grid = 500,  same4all = FALSE)
-    try(KDE.UD <- getverticeshr(KDE.Surface, percent = UDLev))
+    # if(Ext < (scale * 1000 * 2)) {
+    # BExt <- ceiling((scale * 1000 * 3)/(diff(range(SelectedCoords@coords[,1])))) #} else {BExt <- 3}
+    KDE.Surface <- kernelUD(SelectedCoords, h = scale*1000, grid = 500,  same4all = FALSE)
+    try(KDE.UD <- getverticeshr(KDE.Surface, percent = udLev))
     if (isTRUE(class(KDE.UD) == "try-error")) {
       sink("errors.txt", append = T)
       cat(paste("Failed in iteration", i, "with sample", RanNum, sep = " "))
       sink()} else {
-        KDE.UD@proj4string <- SimulatedDistributionsSpatial@proj4string
-        Overlain <- over(SimulatedDistributionsSpatial, KDE.UD)$area
-        Output$InclusionMean <- length(Overlain[!is.na(Overlain)])/nrow(SimulatedDistributionsSpatial@data)
+        KDE.UD@proj4string <- simulatedDistributionsSpatial@proj4string
+        Overlain <- over(simulatedDistributionsSpatial, KDE.UD)$area
+        Output$InclusionMean <- length(Overlain[!is.na(Overlain)])/nrow(simulatedDistributionsSpatial@data)
         return(Output)
         # output_list[j] <- list(Output)
       }
@@ -166,7 +166,7 @@ bootstrapColony <- function(Tracks, SimulatedDistributions, tripID, UDLev = 50, 
     P2$sd <- aggregate(InclusionMean ~ SampleSize, Result, FUN = sd)[,2]
     plot(InclusionMean ~ SampleSize, data = Result, 
          pch = 16, cex = 0.2, col = "darkgray", ylim = c(0, 1),  
-         ylab = "Inclusion", xlab = "Sample Size", main = paste(unique(TracksSpatial$Population), "UDLev", UDLev, sep = "_"))
+         ylab = "Inclusion", xlab = "Sample Size", main = paste(unique(TracksSpatial$Population), "udLev", udLev, sep = "_"))
     yTemp <- c((P2[,2] + P2[,3]), rev(P2[,2] - P2[,3]))
     xTemp <- c(P2[,1], rev(P2[,1]))
     polygon(x = xTemp, y = yTemp, col = "gray93", border = F)
@@ -178,10 +178,10 @@ bootstrapColony <- function(Tracks, SimulatedDistributions, tripID, UDLev = 50, 
     print(RepresentativeValue)
     text(x = 2.5, y = 0.9,paste(round(RepresentativeValue,2), "%", sep = ""), cex = 2, col = "gray45", adj = 0)
   } else{RepresentativeValue <- mean(Result$InclusionMean[Result$SampleSize == max(Result$SampleSize)])   ### if nls is unsuccessful then use mean output for largest sample size
-         Result$RepresentativeValue <- (RepresentativeValue/(UDLev/100))*100
+         Result$RepresentativeValue <- (RepresentativeValue/(udLev/100))*100
          Result$pred <- NA}    ## added by Jono Handley to convert to same scale as nls output
   
   Result$Asymptote <- Asymptote
-  write.table(Result, paste(unique(TracksSpatial$Population), "UDLEv", UDLev, "bootout_temp.csv", sep = "_"), row.names = F, sep = ",")
+  write.table(Result, paste(unique(TracksSpatial$Population), "UDLEv", udLev, "bootout_temp.csv", sep = "_"), row.names = F, sep = ",")
   return(Result)
 }
